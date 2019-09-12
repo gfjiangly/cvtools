5583eb6f79d80f1f22ae07ef7fa1a3d601d9e294
gfjiangly <jgf0719@foxmail.com>
2019-09-12 17:57:31 +0800

    providing the tool for converting dota dataset to croped coco-like dataset.

--- cvtools/label_convert/dota/convert.py
+++ cvtools/label_convert/dota/convert.py
@@ -5,28 +5,43 @@
 # @File    : convert.py
 # @Software: PyCharm
 import cvtools
+from cvtools.data_augs.dota.crop import crop_in_order
 
 
-def convert_dota(mode='train', cls_map='dota_v1.0_cat_id_map.txt'):
-    label_root = 'D:/data/DOTA/{}/labelTxt-v1.0/labelTxt/'.format(mode)
+def convert_dota(label_root, save, mode='train',
+                 cls_map='dota_v1.0_cat_id_map.txt'):
     image_root = 'D:/data/DOTA/{}/images/'.format(mode)
     path_replace = {'\\': '/'}
     dota_to_coco = cvtools.DOTA2COCO(label_root, image_root, cls_map=cls_map,
                                      path_replace=path_replace, box_form='x1y1wh')
     dota_to_coco.convert()
-    dota_to_coco.save_json('{}_dota_x1y1wh_polygen.json'.format(mode))
+    dota_to_coco.save_json(save)
 
 
-def convert_crop_dota(mode='train', cls_map='dota_v1.0_cat_id_map.txt'):
-    crop_label_root = '../../data_augs/dota/{}/crop800x800/labelTxt+crop'.format(mode)
+def convert_crop_dota(crop_label_root, save, mode='train',
+                      cls_map='dota_v1.0_cat_id_map.txt'):
     image_root = 'D:/data/DOTA/{}/images/'.format(mode)
     path_replace = {'\\': '/'}
     dota_to_coco = cvtools.DOTA2COCO(crop_label_root, image_root, cls_map=cls_map,
                                      path_replace=path_replace, box_form='x1y1wh')
     dota_to_coco.convert(use_crop=True)
-    dota_to_coco.save_json('{}_crop800x800_dota_x1y1wh_polygen.json'.format(mode))
+    dota_to_coco.save_json(save)
 
 
 if __name__ == '__main__':
-    convert_crop_dota(mode='train')
+    cvtools._DEBUG = False
+    mode = 'train'
+
+    label_root = 'D:/data/DOTA/{}/labelTxt-v1.0/labelTxt/'.format(mode)
+    save = 'D:/data/DOTA/annotations/{}_dota+original.json'.format(mode)
+    convert_dota(label_root, save, mode=mode)
+
+    ann_file = 'D:/data/DOTA/annotations/{}_dota+original.json'.format(mode)
+    save = 'D:/data/DOTA/crop/{}'.format(mode)
+    crop_in_order(ann_file, save, mode, vis=False)
+
+    crop_label_root = 'D:/data/DOTA/crop/{}/labelTxt+crop'.format(mode)
+    save = 'D:/data/DOTA/annotations/{}_dota+crop.json'.format(mode)
+    convert_crop_dota(crop_label_root, save, mode=mode)
+
 
