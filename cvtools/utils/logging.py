1e5693bce321c5242d299a5e1d5f94d761facbdb
gfjiangly <jgf0719@foxmail.com>
2019-09-21 15:44:31 +0800

    add logging.

---
+++ cvtools/utils/logging.py
@@ -0,0 +1,26 @@
+# -*- encoding:utf-8 -*-
+# @Time    : 2019/9/20 22:09
+# @Author  : gfjiang
+# @Site    : 
+# @File    : logging.py
+# @Software: PyCharm
+import logging
+
+
+def get_logger(log_level, name=None):
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s', level=log_level)
+    logger = logging.getLogger(name)
+    return logger
+
+
+def logger_file_handler(logger,
+                        filename=None,
+                        mode='w',
+                        level=logging.INFO):
+    file_handler = logging.FileHandler(filename, mode)
+    file_handler.setFormatter(
+        logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    file_handler.setLevel(level)
+    logger.addHandler(file_handler)
+    return logger
