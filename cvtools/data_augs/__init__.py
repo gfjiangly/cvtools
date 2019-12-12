79d518397b8ce1ec5abb9f7fffc0a665860b8fbd
gfjiangly <jgf0719@foxmail.com>
2019-12-12 11:13:50 +0800

    update export.

--- cvtools/data_augs/__init__.py
+++ cvtools/data_augs/__init__.py
@@ -3,10 +3,27 @@
 # time     : 2019/7/10 16:11
 # e-mail   : jgf0719@foxmail.com
 # software : PyCharm
+from .augmentations import Compose
+from .augmentations import ToAbsoluteCoords, ToPercentCoords, ConvertFromInts
+from .augmentations import (RandomSaturation, RandomHue, RandomLightingNoise,
+                            ConvertColor, RandomContrast, RandomBrightness)
+from .augmentations import PhotometricDistort
+from .augmentations import RandomSampleCrop
+from .augmentations import Expand
+from .augmentations import (RandomRotate, RandomVerMirror, RandomHorMirror,
+                            RandomMirror)
 from .crop_in_order import CropInOder
-from .augmentations import RandomRotate, RandomVerMirror, RandomHorMirror, RandomMirror
+
 
 __all__ = [
+    'Compose',
+    'ToAbsoluteCoords', 'ToPercentCoords', 'ConvertFromInts',
+    'RandomSaturation', 'RandomHue', 'RandomLightingNoise', 'ConvertColor',
+    'RandomContrast', 'RandomBrightness',
+    'PhotometricDistort',
+    'RandomSampleCrop',
+    'Expand',
+    'RandomRotate', 'RandomVerMirror', 'RandomHorMirror', 'RandomMirror',
+    # outline
     'CropInOder',
-    'RandomRotate', 'RandomVerMirror', 'RandomHorMirror', 'RandomMirror'
 ]
--- cvtools/data_augs/generateor_thread.py
+++ cvtools/data_augs/generateor_thread.py
@@ -5,26 +5,33 @@
 # software : PyCharm
 
 import os
-import cv2
+import os.path as osp
+import numpy as np
 import time
 from tqdm import tqdm
 from multiprocessing import Pool
 import threading
 from time import sleep, ctime
 
-from augmentations import *
-from utils.file_utils import read_files_to_list, makedirs
-from utils.image_utils import draw_rect_test_labels
+import cvtools
+from cvtools.utils.file import read_files_to_list, makedirs
+from cvtools.utils.image import draw_rect_test_labels
 
 
 class DataAugmentation(object):
-    def __init__(self, mean=(127, 127, 127)):     # mean值应该通过对自己的数据集聚类得到，但是似乎影响不大，暂时没修改
+    def __init__(self, mean=(127, 127, 127)):
+        """
+
+        Args:
+            mean: 如果没有预训练模型，通过对自己的数据集聚类得到，但是似乎影响不大，暂时没修改
+            如使用预训练模型，则使用预训练模型使用的mean
+        """
         self.mean = mean
-        self.augment = Compose([
-            ConvertFromInts(),      # int->np.float32, for image
-            PhotometricDistort(),   # 光度变形
-            Expand(self.mean),      # 概率图像扩展
-            RandomSampleCrop()      # 随机裁剪
+        self.augment = cvtools.Compose([
+            cvtools.ConvertFromInts(),      # int->np.float32, for image
+            cvtools.PhotometricDistort(),   # 光度变形
+            cvtools.Expand(self.mean),      # 概率图像扩展
+            cvtools.RandomSampleCrop()      # 随机裁剪
         ])
 
     def __call__(self, img, boxes, labels):
@@ -63,7 +70,7 @@ class myThread(threading.Thread):
             def augment_one_image(line):
                 nonlocal annts_lines
                 line = line.strip().split()
-                image_name = line[0]
+                file_path = line[0]
                 boxes = []
                 classes = []
                 for label_str in line[1:]:
@@ -72,21 +79,23 @@ class myThread(threading.Thread):
                     classes.append(int(bbox_cls_str[4]))
                 boxes = np.array(boxes)
                 classes = np.array(classes)
-                new = save_path + image_name.replace('\\', '/').replace('.jpg', '').split('/')[-1] + '_{index}.jpg'
+                new_name = osp.splitext(file_path.split(os.sep)[-1])[0]
+                new = save_path + new_name.replace('.jpg', '') + '_{index}.jpg'
                 for im_index in range(1, 5):    # 每张图片增强出4张
                     new_image_name = new.format(index=im_index)
                     if not os.path.isfile(new_image_name):
-                        # im = cv2.imread(image_name)  # BGR 如果路径中含中文，读取不成功，img为None
-                        im = cv2.imdecode(np.fromfile(image_name, dtype=np.uint8), cv2.IMREAD_COLOR)
-                        img, boxes_trans, classes_trans = transfer(im, boxes, classes)
+                        im = cvtools.imread(file_path)
+                        img, boxes_trans, classes_trans = transfer(im, boxes,
+                                                                   classes)
                         boxes_trans = boxes_trans.astype(np.int32)
                         classes_trans = classes_trans.astype(np.int32)
                         # print('save %s...' % new_image_name)
-                        cv2.imwrite(new_image_name, img)
+                        cvtools.imwrite(img, new_image_name)
                         threadLock.acquire()
                         annts_lines += new_image_name + ' '
                         for box, cls in zip(boxes_trans, classes_trans):
-                            annts_lines += ','.join(map(str, box)) + ',' + str(cls) + ' '
+                            annts_lines += ','.join(map(str, box)) + \
+                                           ',' + str(cls) + ' '
                         annts_lines += '\n'
                         threadLock.release()
             # pool.apply_async(augment_one_image, args=(line,))
@@ -110,8 +119,12 @@ if __name__ == '__main__':
     # root = 'labels/train/'
     # data_list = ['elevator_20181230_convert_train.txt']
     root = 'labels/train/'
-    data_list = ['elevator_20181230_convert_train.txt', 'elevator_20181231_convert_train.txt',
-                 'elevator_20190106_convert_train.txt', 'person_7421_train.txt']
+    data_list = [
+        'elevator_20181230_convert_train.txt',
+        'elevator_20181231_convert_train.txt',
+        'elevator_20190106_convert_train.txt',
+        'person_7421_train.txt'
+    ]
     dataset = read_files_to_list(root, data_list)
     # 单线程处理82张原始图需要89s
     # 2线程处理82张原始图需要69s
