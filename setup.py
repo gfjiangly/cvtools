bacb99d09ea640f63f54b64369fb7e22e834221d
gfjiangly <jgf0719@foxmail.com>
2019-12-14 09:27:42 +0800

    Cython is build-time dependency and is removed from install_requires.

--- setup.py
+++ setup.py
@@ -13,10 +13,17 @@ from Cython.Distutils import build_ext  # noqa: E402
 
 
 install_requires = [
-    'Cython', 'numpy>=1.11.1', 'opencv-python', 'pillow', 'matplotlib',
-    'tqdm', 'pyyaml', 'terminaltables', 'mmcv>=0.2.13',
+    'numpy>=1.11.1',
+    'opencv-python',
+    'pillow',
+    'matplotlib',
+    'tqdm',
+    'pyyaml',
+    'terminaltables',
+    'mmcv>=0.2.13',
     # 'scikit-learn>=0.21.2',
-    'shapely>=1.6.4', 'terminaltables',
+    'shapely>=1.6.4',
+    'terminaltables',
 ]
 
 
