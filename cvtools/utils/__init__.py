50080bb49c5331e415a15f24bedf431b7af935e1
gfjiangly <jgf0719@foxmail.com>
2019-12-14 10:35:13 +0800

    fix for PEP8.

--- cvtools/utils/__init__.py
+++ cvtools/utils/__init__.py
@@ -4,16 +4,22 @@
 # e-mail   : jgf0719@foxmail.com
 # software : PyCharm
 
-from .file import (readlines, read_files_to_list, write_list_to_file, read_files_to_list,
-                   get_files_list, get_images_list, split_list, split_dict, split_data, replace_filename_space,
-                   check_rept, makedirs, sample_label_from_images, read_key_value, folder_name_replace,
-                   files_name_replace, load_json, save_json, folder_name_replace, files_name_replace,
-                   check_file_exist, write_key_value, strwrite, isfile_casesensitive, is_image_file)
-from .image import (imread, imwrite, draw_boxes_texts, draw_class_distribution, draw_hist)
-from .boxes import (x1y1wh_to_x1y1x2y2, x1y1x2y2_to_x1y1wh, xywh_to_x1y1x2y2, x1y1x2y2_to_xywh,
-                    x1y1wh_to_xywh, rotate_rect, rotate_rects, xywha_to_x1y1x2y2x3y3x4y4)
+from .file import (readlines, read_files_to_list, write_list_to_file,
+                   read_files_to_list, get_files_list, get_images_list,
+                   split_list, split_dict, split_data, replace_filename_space,
+                   check_rept, makedirs, sample_label_from_images,
+                   read_key_value, folder_name_replace, files_name_replace,
+                   load_json, save_json, folder_name_replace,
+                   files_name_replace, check_file_exist, write_key_value,
+                   strwrite, isfile_casesensitive, is_image_file)
+from .image import (imread, imwrite, draw_boxes_texts, draw_class_distribution,
+                    draw_hist)
+from .boxes import (x1y1wh_to_x1y1x2y2, x1y1x2y2_to_x1y1wh, xywh_to_x1y1x2y2,
+                    x1y1x2y2_to_xywh, x1y1wh_to_xywh, rotate_rect,
+                    xywha_to_x1y1x2y2x3y3x4y4)
 from .timer import (Timer, get_time_str)
-from .label import (read_arcsoft_txt_format, read_jiang_txt, read_yuncong_detect_file)
+from .label import (read_arcsoft_txt_format, read_jiang_txt,
+                    read_yuncong_detect_file)
 from .iou import (box_iou, bbox_overlaps)
 # from .cluster import k_means_cluster, DBSCAN_cluster
 from .misc import (is_str, iter_cast, list_cast, tuple_cast, is_seq_of,
@@ -21,27 +27,32 @@ from .misc import (is_str, iter_cast, list_cast, tuple_cast, is_seq_of,
 from .logging import get_logger, logger_file_handler
 
 
-__all__ = ['readlines', 'read_files_to_list', 'write_list_to_file', 'read_files_to_list',
-           'get_files_list', 'get_images_list', 'split_list', 'split_dict', 'split_data',
-           'replace_filename_space', 'check_rept', 'makedirs', 'sample_label_from_images',
-           'read_key_value', 'folder_name_replace', 'files_name_replace', 'load_json',
-           'save_json', 'folder_name_replace', 'files_name_replace', 'check_file_exist',
-           'write_key_value', 'strwrite', 'isfile_casesensitive', 'is_image_file',
+__all__ = [
+    'readlines', 'read_files_to_list', 'write_list_to_file',
+    'read_files_to_list', 'get_files_list', 'get_images_list', 'split_list',
+    'split_dict', 'split_data', 'replace_filename_space', 'check_rept',
+    'makedirs', 'sample_label_from_images', 'read_key_value',
+    'folder_name_replace', 'files_name_replace', 'load_json', 'save_json',
+    'folder_name_replace', 'files_name_replace', 'check_file_exist',
+    'write_key_value', 'strwrite', 'isfile_casesensitive', 'is_image_file',
 
-           'imread', 'imwrite', 'draw_boxes_texts', 'draw_class_distribution', 'draw_hist',
+    'imread', 'imwrite', 'draw_boxes_texts', 'draw_class_distribution',
+    'draw_hist',
 
-           'x1y1wh_to_x1y1x2y2', 'x1y1x2y2_to_x1y1wh', 'xywh_to_x1y1x2y2', 'x1y1x2y2_to_xywh',
-           'x1y1wh_to_xywh', 'rotate_rect', 'rotate_rects', 'xywha_to_x1y1x2y2x3y3x4y4',
+    'x1y1wh_to_x1y1x2y2', 'x1y1x2y2_to_x1y1wh', 'xywh_to_x1y1x2y2',
+    'x1y1x2y2_to_xywh', 'x1y1wh_to_xywh', 'rotate_rect',
+    'xywha_to_x1y1x2y2x3y3x4y4',
 
-           'Timer', 'get_time_str',
+    'Timer', 'get_time_str',
 
-           'read_arcsoft_txt_format', 'read_jiang_txt', 'read_yuncong_detect_file',
+    'read_arcsoft_txt_format', 'read_jiang_txt', 'read_yuncong_detect_file',
 
-           'box_iou', 'bbox_overlaps',
+    'box_iou', 'bbox_overlaps',
 
-           # 'k_means_cluster', 'DBSCAN_cluster',
+    # 'k_means_cluster', 'DBSCAN_cluster',
 
-           'is_str', 'iter_cast', 'list_cast', 'tuple_cast', 'is_seq_of', 'is_list_of',
-           'is_tuple_of', 'slice_list', 'concat_list',
+    'is_str', 'iter_cast', 'list_cast', 'tuple_cast', 'is_seq_of', 'is_list_of',
+    'is_tuple_of', 'slice_list', 'concat_list',
 
-           'get_logger', 'logger_file_handler']
+    'get_logger', 'logger_file_handler',
+]
