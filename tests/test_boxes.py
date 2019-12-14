04f684e1a505676565b99d2001e571fc29a1dc82
gfjiangly <jgf0719@foxmail.com>
2019-12-14 11:21:55 +0800

    fix tests.

--- tests/test_boxes.py
+++ tests/test_boxes.py
@@ -9,9 +9,15 @@ import numpy as np
 import cvtools
 
 
-def test_rotate_rects():
-    rects = np.array([[100, 200, 300, 400], [101, 201, 301, 401]])
-    center = np.array([[200, 300], [201, 301]])
-    angle = np.array([45, 30])
-    new_boxes = cvtools.rotate_rects(rects, center, angle)
-    print(new_boxes)
+def test_rotate_rect():
+    # rects = np.array([[100, 200, 300, 400], [101, 201, 301, 401]])
+    # center = np.array([[200, 300], [201, 301]])
+    # angle = np.array([45, 30])
+    # new_boxes = cvtools.rotate_rects(rects, center, angle)
+    # print(new_boxes)
+
+    rect = [100, 200, 300, 400]
+    center = [200, 300]
+    angle = 45
+    new_rect = cvtools.rotate_rect(rect, center, angle)
+    print(new_rect)
