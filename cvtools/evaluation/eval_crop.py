754129b3cdf3b8d16d66b395608325e6811d819c
gfjiangly <jgf0719@foxmail.com>
2019-12-27 17:55:01 +0800

    Fix some issues.

--- cvtools/evaluation/eval_crop.py
+++ cvtools/evaluation/eval_crop.py
@@ -7,37 +7,60 @@
 import numpy as np
 
 import cvtools
-from cvtools.evaluation.merge_dets import ConvertGT2Dets, MergeCropDetResults
+from cvtools.evaluation.merge_dets import MergeCropDetResults
 from cvtools.evaluation.mean_ap import eval_map
 
 
-def eval_crop(ann_file, crop_ann_file):
-    gt = ConvertGT2Dets(crop_ann_file)
-    results = gt.convert()
-    dets = MergeCropDetResults(crop_ann_file, results)
-    merge_dets = dets.merge()
+class EvalCropQuality(object):
+    """此类设计目前不够完善，convert_crop_gt应隐藏在内部"""
+    def __init__(self,
+                 ann_file,
+                 crop_ann_file,
+                 convert_crop_gt=None,
+                 nms_method=cvtools.py_cpu_nms,
+                 num_coors=4):
+        assert num_coors in (4, 8), "不支持的检测位置表示"
+        self.nms_method = nms_method
+        self.num_coors = num_coors
+        self.coco = cvtools.COCO(ann_file)
+        self.anns = self.coco.anns
+        if convert_crop_gt is None:
+            gt = cvtools.COCO2Dets(crop_ann_file)
+        else:
+            gt = convert_crop_gt(crop_ann_file)
+        self.results = gt.convert()
+        dets = MergeCropDetResults(crop_ann_file, self.results, self.num_coors)
+        self.merge_dets = dets.merge(nms_method)
 
-    coco = cvtools.COCO(ann_file)
+    def handel_ann(self, ann):
+        if self.num_coors == 4:
+            bboxes = cvtools.x1y1wh_to_x1y1x2y2(ann['bbox'])
+        elif self.num_coors == 8:
+            bboxes = ann['segmentation'][0]
+        else:
+            raise RuntimeError('不支持的坐标数！')
+        return bboxes
 
-    gt_bboxes = []
-    gt_labels = []
-    det_results = []
-    for img_id, img_info in coco.imgs.items():
-        ann_ids = coco.getAnnIds(imgIds=[img_id])
-        anns = coco.loadAnns(ann_ids)
-        gt_bboxes.append(np.array([cvtools.x1y1wh_to_x1y1x2y2(ann['bbox'])
-                                   for ann in anns]))
-        gt_labels.append(np.array([ann['category_id'] for ann in anns]))
-        try:
-            bboxes, labels, scores = merge_dets[img_info['file_name']]
-            bboxes = np.hstack([bboxes, scores[:, np.newaxis]])
-        except ValueError:
-            bboxes, labels = [], []
-        dets = [[] for _ in range(len(coco.cats))]
-        for cls, bbox in zip(labels, bboxes):
-            dets[cls-1].append(bbox)
-        for cls, det in enumerate(dets):
-            dets[cls] = np.array(det) if len(det) > 0 else np.empty((0, 5))
-        det_results.append(dets)
+    def eval(self):
+        gt_bboxes = []
+        gt_labels = []
+        det_results = []
+        for img_id, img_info in self.coco.imgs.items():
+            ann_ids = self.coco.getAnnIds(imgIds=[img_id])
+            anns = self.coco.loadAnns(ann_ids)
+            gt_bboxes.append(np.array([self.handel_ann(ann) for ann in anns]))
+            gt_labels.append(np.array([ann['category_id'] for ann in anns]))
+            try:
+                bboxes, labels, scores = self.merge_dets[img_info['file_name']]
+                bboxes = np.hstack([bboxes, scores[:, np.newaxis]])
+            except ValueError:
+                bboxes, labels = [], []
+            dets = [[] for _ in range(len(self.coco.cats))]
+            for cls, bbox in zip(labels, bboxes):
+                dets[cls - 1].append(bbox)
+            for cls, det in enumerate(dets):
+                dets[cls] = np.array(det) if len(det) > 0 else \
+                    np.empty((0, self.num_coors+1))
+            det_results.append(dets)
 
-    eval_map(det_results, gt_bboxes, gt_labels, dataset='dota')
+        eval_map(det_results, gt_bboxes, gt_labels, dataset='dota')
--- cvtools/evaluation/merge_dets.py
+++ cvtools/evaluation/merge_dets.py
@@ -5,8 +5,6 @@
 # @Software: PyCharm
 import numpy as np
 from collections import defaultdict
-from multiprocessing import Pool
-from tqdm import tqdm
 import os.path as osp
 
 import cvtools
@@ -14,12 +12,12 @@ import cvtools
 
 class MergeCropDetResults(object):
 
-    def __init__(self, anns_file, results, num_coors=4, num_workers=0):
+    def __init__(self, anns_file, results, num_coors=4):
         """
 
         Args:
             anns_file (str): DOTA-COCO兼容格式, img_info中必须含crop字段
-            results (str): 子图检测结果，保存在文件中，
+            results (str or dict): 子图检测结果，保存在文件中，
                 results:
                 {
                     image_id: dets,  # image_id必须是anns中有效的id
@@ -34,7 +32,6 @@ class MergeCropDetResults(object):
                 }，
                 位置坐标支持左上角右下角两个点表示和四个点的表示方式
             num_coors (int): 4表示左上角右下角box， 8表示四个点的box
-            num_workers (int): 进程数，默认值0为表示不使用多进程
         """
         assert num_coors in (4, 8), "不支持的检测位置表示"
         self.coco = cvtools.COCO(anns_file)
@@ -44,7 +41,6 @@ class MergeCropDetResults(object):
         else:
             self.results = results
         self.num_coors = num_coors
-        self.num_workers = num_workers
         self.img_to_results = {}    # merge返回结果保存
 
     def crop_bbox_map_back(self, bb, crop_start):
@@ -53,16 +49,16 @@ class MergeCropDetResults(object):
         original_bb = bb.reshape(-1, 2) + np.array(crop_start).reshape(-1, 2)
         return original_bb.reshape(bb_shape)
 
-    def match_part_img_results(self, results, coor_len=4):
+    def match_part_img_results(self, results):
         """结合子图的结果，映射回原图，应用nms, 生成一张整图的结果"""
         img_to_results = defaultdict(list)
         for image_id, dets in results.items():
             img_info = self.coco.imgs[image_id]
             labels = cvtools.concat_list(
                 [[j]*len(det) for j, det in dets.items()])    # label从0开始
-            scores = cvtools.concat_list([det[:, coor_len]
+            scores = cvtools.concat_list([det[:, self.num_coors]
                                           for det in dets.values()])
-            bboxes = np.vstack([det[:, :coor_len] for det in dets.values()
+            bboxes = np.vstack([det[:, :self.num_coors] for det in dets.values()
                                 if len(det) > 0])
             if 'crop' in img_info:
                 bboxes = self.crop_bbox_map_back(bboxes, img_info['crop'][:2])
@@ -87,24 +83,7 @@ class MergeCropDetResults(object):
                 ...
             }
         """
-        img_to_results = defaultdict(list)
-        if self.num_workers > 0:
-            pool = Pool(processes=self.num_workers)
-            num = len(self.anns) // self.num_workers
-            anns_group = [self.anns[i:i + num]
-                          for i in range(0, len(self.anns), num)]
-            results_group = [self.results[i:i + num] for i in
-                             range(0, len(self.results), num)]
-            res = []
-            for anns, results in tqdm(zip(anns_group, results_group)):
-                res.append(pool.apply_async(self.match_part_img_results,
-                                            args=(anns, self.results,)))
-            pool.close()
-            pool.join()
-            for item in res:
-                img_to_results.update(item.get())
-        else:
-            img_to_results = self.match_part_img_results(self.results)
+        img_to_results = self.match_part_img_results(self.results)
         for filename, result in img_to_results.items():
             bboxes = np.vstack([bb[0] for bb in result]).astype(np.int)
             labels = np.hstack([bb[1] for bb in result])
@@ -126,7 +105,7 @@ class MergeCropDetResults(object):
         return cat_results
 
     def save_dota_det_format(self, save_path):
-        class_names = [cat['name'] for cat in self.coco.cats]
+        class_names = [cat['name'] for cat in self.coco.cats.values()]
         self.cat_results = self.img_results_to_cat_results(self.img_to_results)
         path = osp.join(save_path, 'Task1_{}.txt')
         for cat_id, result in self.cat_results.items():
@@ -136,40 +115,5 @@ class MergeCropDetResults(object):
                 bbox = list(map(str, list(bbox)))
                 score = str(round(score, 3))
                 lines.append(' '.join([filename] + [score] + bbox))
-            cvtools.write_list_to_file(lines, path.format(class_names[cat_id]))
-
-
-class ConvertGT2Dets(object):
-    """
-        将DOTA-COCO兼容格式GT转成检测结果表达形式results，保存成pkl
-        results:
-        {
-            image_id: dets,  # image_id必须是anns中有效的id
-            image_id: dets,
-            ...
-        }
-        dets:
-        {
-            类别：[[位置坐标，得分], [...], ...],
-            类别: [[位置坐标，得分], [...], ...],
-            ...
-        }，
-    """
-    def __init__(self, anns_file):
-        self.coco = cvtools.COCO(anns_file)
-        self.results = defaultdict()    # 动态创建嵌套字典
-
-    def convert(self, to_file=None):
-        for img_id, img_info in self.coco.imgs.items():
-            dets = defaultdict(list)
-            ann_ids = self.coco.getAnnIds(imgIds=[img_id])
-            anns = self.coco.loadAnns(ann_ids)
-            for ann in anns:
-                x1y1x2y2 = cvtools.x1y1wh_to_x1y1x2y2(ann['bbox'])
-                dets[ann['category_id']].append(x1y1x2y2 + [1.])
-            for cls, det in dets.items():
-                dets[cls] = np.array(det)
-            self.results[img_id] = dets
-        if to_file is not None:
-            cvtools.dump_pkl(self.results, to_file)
-        return self.results
+            cvtools.write_list_to_file(
+                lines, path.format(class_names[cat_id-1]))
